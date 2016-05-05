2e8b00585bb5aba99a3b5d0da24170b3b0f8b1da
MarianaBraga <marianabraga7495@gmail.com>
2016-05-05 11:23:26 -0300

    first commit

---
+++ app/src/main/java/com/example/alu201416282/healthymeal/MapsActivity.java
@@ -0,0 +1,13 @@
+package com.example.alu201416282.healthymeal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
